9e9258a7490b93b4f6a09c5edb3ce3a598b301f4
Abraham Sinsard <asinsard@z2r2p7.42lyon.fr>
2024-12-11 22:43:36 +0100

    first step of parsing

--- parsing.c
+++ parsing.c
@@ -6,13 +6,28 @@
 /*   By: asinsard <asinsard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 18:31:13 by asinsard          #+#    #+#             */
-/*   Updated: 2024/12/11 18:32:00 by asinsard         ###   ########lyon.fr   */
+/*   Updated: 2024/12/11 22:43:12 by asinsard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_verif_map()
 {
-	int	x;
-	int	y;
+	
 	
 }
+
+// is the map is surrounded by wall ?
+
+// is the map is rectangular ?
+
+// is the maps line have same lenght ?
+
+// is the file have map ?
+
+// the map must have 1 E and 1 P ?
+
+// the map must have at least 1 C ?
+
+// is the E is available by the P ?
+
+// is the C are whithin reach of P ?
